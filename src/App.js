796584ae08dc0c7205bd1a148b373c7b9dc0c3e2
monicasalas86 <monicsalas167@gmail.com>
2021-09-17 16:26:37 -0500

    order form functional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios'
 
+import Form from './components/PizzaForm'
+import schema from './components/FormSchema'
+import * as yup from 'yup'
+
 
 const initialFormValues = {
   name: '',
@@ -39,8 +43,46 @@ const App = () => {
       })
   }
 
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      toppings: ['pepperoni', 'sausage', 'mushroom', 'pepper'].filter(topping => !!formValues[topping]),
+      special: formValues.special.trim()
+    }
+    postNewOrder(newOrder);
+  }
+
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
+
   return (
-    <h1>Lambda Eats</h1>
+    <div>
+      <h1>Lambda Eats</h1>
+      <Form
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+    </div>
   )
 };
 export default App;
