4112f55aa9eb0662c964cf6e592cd936d5779d3e
monicasalas86 <monicsalas167@gmail.com>
2021-09-17 16:11:40 -0500

    created form submit function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from "react";
-import Form from './components/PizzaForm'
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
+
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  pepperoni: false,
+  sausage: false,
+  mushroom: false,
+  pepper: false,
+  special: ''
+}
+
+const initialFormErrors = {
+  name: '',
+  size: ''
+}
+
+const initialOrder = []
+const initialDisabled = true
+
 
 const App = () => {
+
+  const [order, setOrder] = useState(initialOrder)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] =useState(initialDisabled)
+
+  const postNewOrder = newOrder => {
+    axios.post('https://reqres.in/api/orders', newOrder)
+      .then(res => {
+        setOrder([res.data.data, ...order])
+        setFormValues(initialFormValues)
+      }).catch(err => {
+        console.log(err)
+        setFormValues(initialFormValues)
+      })
+  }
+
   return (
-    <Form/>
-  );
+    <h1>Lambda Eats</h1>
+  )
 };
 export default App;
